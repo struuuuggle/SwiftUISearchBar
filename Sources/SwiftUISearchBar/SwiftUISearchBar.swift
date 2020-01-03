e05dc36e2d749bbc83ccc06c727d12df2d62033a
Mikiya Abe <struuuuggle@gmail.com>
2020-01-03 17:29:56 +0900

    Add public initializer to be used from anywhere

--- Sources/SwiftUISearchBar/SwiftUISearchBar.swift
+++ Sources/SwiftUISearchBar/SwiftUISearchBar.swift
@@ -14,11 +14,25 @@ public struct SwiftUISearchBar: View {
     @State private var showCancelButton: Bool = false
 
     @Binding public var text: String
-    public let placeholder: String?
+    public let placeholder: String
     public var onTap: (() -> Void)?
     public var onEdit: (() -> Void)?
     public var onCommit: ((String) -> Void)?
     public var onCancel: (() -> Void)?
+    
+    public init(text: Binding<String>, placeholder: String = "",
+                onTap: (() -> Void)? = nil,
+                onEdit: (() -> Void)? = nil,
+                onCommit: ((String) -> Void)? = nil,
+                onCancel: (() -> Void)? = nil
+                ) {
+        self._text = text
+        self.placeholder = placeholder
+        self.onTap = onTap
+        self.onEdit = onEdit
+        self.onCommit = onCommit
+        self.onCancel = onCancel
+    }
 
     public var body: some View {
         HStack {
